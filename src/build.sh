a369bead978e53f9c39303e06347ab78ae2edbe7
javier.figueroa <javier.figueroa@192.168.1.104>
2011-04-02 16:09:38 -0400

    cleaned

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 kill `ps -ef | grep 7777 | egrep -v grep | awk '{print $2}'`
 rmiregistry 7777&
-mv ?*.tar proj2.tar
-tar xvf proj2.tar; rm *.class
+mv ?*.tar proj3.tar
+tar xvf proj3.tar; rm *.class
 rm *.log
 javac *.java; java start
